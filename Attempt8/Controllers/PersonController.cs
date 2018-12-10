28c2055c202d587d33b5ebfeb4c70c7bf804ea50
unknown <shehzadikomal303@gmail.com>
2018-12-10 15:33:42 +0500

    third commit

--- Attempt8/Controllers/PersonController.cs
+++ Attempt8/Controllers/PersonController.cs
@@ -84,7 +84,7 @@ namespace Attempt8.Models
                     first.Gender = null;
                     db.Profiles.Add(first);
                     db.SaveChanges();
-                    return RedirectToAction("Create", "Person");
+                    return RedirectToAction("ViewClass", "Class");
                 }
                 return View();
             }
@@ -134,12 +134,125 @@ namespace Attempt8.Models
             }
             if(DLSInterface.loggedEmail != null)
             {
-                return RedirectToAction("Login", "Person");
+                return RedirectToAction("ViewClass", "Class");
             }
             return View();
         }
 
-       
+        public ActionResult AddClass()
+        {
+            SE_ProjectEntities db = new SE_ProjectEntities();
+            List<SchoolTbl> SchoolList = db.SchoolTbls.ToList();
+            ViewBag.SchoolList = new SelectList(SchoolList, "Id", "Name");
+
+            ViewBag.Warn = "";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddClass(ViewClassViewModels model)
+        {
+            try
+            {
+                SE_ProjectEntities db = new SE_ProjectEntities();
+
+                List<SchoolTbl> SchoolList = db.SchoolTbls.ToList();
+                ViewBag.SchoolList = new SelectList(SchoolList, "Id", "Name");
+
+                if (DLSInterface.loggedEmail == null)
+                {
+                    ViewBag.Warn = "Login First";
+                    return View();
+                }
+                else if (db.TeacherTbls.Any(t => t.Email.Equals(DLSInterface.loggedEmail)))
+                {
+                    int teacherId = db.TeacherTbls.Where(t => t.Email.Equals(DLSInterface.loggedEmail)).FirstOrDefault().Id;
+                    int classId = db.ClassTbls.Where(c => c.Name.Equals(model.className)).FirstOrDefault().Id;
+                    string classCode = db.ClassTbls.Where(c => c.Name.Equals(model.className)).FirstOrDefault().Code;
+
+                    bool isExist = false;
+                    foreach (TeacherClassOTM tc in db.TeacherClassOTMs)
+                    {
+                        if (tc.Teacher_Id == teacherId && tc.Class_Id == classId)
+                        {
+                            isExist = true;
+                        }
+                    }
+                    if(isExist)
+                    {
+                        ViewBag.Warn = "Already Registered in Class";
+                        return View();
+                    }
+                    else
+                    {
+                        TeacherClassOTM tc = new TeacherClassOTM();
+                        tc.Teacher_Id = teacherId;
+                        tc.Class_Id = classId;
+                        if(classCode.Equals(model.classCode))
+                        {
+                            db.TeacherClassOTMs.Add(tc);
+                            db.SaveChanges();
+                            ViewBag.Warn = "Success!";
+                            return View();
+                        }
+                        else
+                        {
+                            ViewBag.Warn = "Invalid code!";
+                            return View();
+                        }
+                        
+                    }
+                }
+                else if (db.StudentTbls.Any(t => t.Email.Equals(DLSInterface.loggedEmail)))
+                {
+                    int studentId = db.StudentTbls.Where(s => s.Email.Equals(DLSInterface.loggedEmail)).FirstOrDefault().Id;
+                    int classId = db.ClassTbls.Where(c => c.Name.Equals(model.className)).FirstOrDefault().Id;
+                    string classCode = db.ClassTbls.Where(c => c.Name.Equals(model.className)).FirstOrDefault().Code;
+
+                    bool isExist = false;
+                    foreach (StudentClassOTM sc in db.StudentClassOTMs)
+                    {
+                        if (sc.Student_Id == studentId && sc.Class_Id == classId)
+                        {
+                            isExist = true;
+                            break;
+                        }
+                    }
+                    if(isExist)
+                    {
+                        ViewBag.Warn = "Already Registered in Class";
+                        return View();
+                    }
+                    else
+                    {
+                        StudentClassOTM sc = new StudentClassOTM();
+                        sc.Student_Id = studentId;
+                        sc.Class_Id = classId;
+
+                        if (classCode.Equals(model.classCode))
+                        {
+                            db.StudentClassOTMs.Add(sc);
+                            db.SaveChanges();
+                            ViewBag.Warn = "Success!";
+                            return View();
+                        }
+                        else
+                        {
+                            ViewBag.Warn = "Invalid code!";
+                            return View();
+                        }
+                    }
+                }
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+    
+
         // GET: Person/Edit/5
         public ActionResult Edit(int id)
         {
